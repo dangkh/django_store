8f7bd6a3322b7b09ef00a341a728def1042e1241
dangkh <dangkh_58@vnu.edu.vn>
2018-11-22 01:14:36 +0700

    finish home page

--- mystore/views.py
+++ mystore/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from mystore.models import City, Store, Image
+from random import randint
 
 def index(request):
+
     citys = City.objects.all()
     stores = Store.objects.all()
-    context = {'citys': citys, 'stores': stores}
+    city = []
+    store = []
+    if stores:
+      random_index2 = randint(0, stores.count() - 1)
+      store = stores[random_index2]
+    if citys:
+      random_index1 = randint(0, citys.count() - 1)
+      city = citys[random_index1]
+
+    context = {'city': city, 'store': store}
     return render(request, 'mystore/index.html', context)
